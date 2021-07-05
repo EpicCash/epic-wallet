f58cdcfd99dbad12020b6ffa5fe80ef1cd0fd2d6
Johannes Hahn <unitone@bastelbeat.de>
2021-07-05 10:37:50 +0200

    remove mod cmd for mobile

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+use std::os::raw::{c_char};
+use std::ffi::{CString, CStr};
+//use epic_wallet_config as config;
+//use epic_wallet_util::epic_api as api;
+//use epic_wallet_util::epic_util as util;
 
-use epic_wallet_config as config;
-use epic_wallet_util::epic_api as api;
-use epic_wallet_util::epic_util as util;
-
-pub mod cmd;
+//pub mod cmd;
