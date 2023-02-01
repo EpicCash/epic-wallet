b495cdb76ed7c76b347a6204f03ca4ba98f4f5c7
Jualns <josilva602@gmail.com>
2023-02-01 11:40:23 -0300

    feat: add implementarion to Header struct

--- impls/src/adapters/emoji_map.rs
+++ impls/src/adapters/emoji_map.rs
@@ -1,4 +1,23 @@
+// Copyright 2023 The Epic Developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use emoji::{self, Emoji};
+
+/// The divider emoji is the brick
+/// NEED BE UNIQUE
+/// DON'T REUSE THIS EMOJI
+pub const EMOJI_DIVIDER: Emoji = emoji::travel_and_places::place_building::BRICK;
 
 /// All the emojis we use to encode and decode the Slate into a String of emojis
 pub static EMOJI_MAP: [Emoji; 1024] = [
